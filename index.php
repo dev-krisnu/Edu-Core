ca4befcf77110573fe5527dcdc330154352fead2
KRRISH JESWAR <krrishjeswar01@gmail.com>
2026-08-08 23:27:47 +0530

    added the divs of index.php

--- index.php
+++ index.php
@@ -7,21 +7,17 @@
     <title>Edu-Core</title>
   </head>
   <body>
-    <h1>This is the main page </h1>
-      <div class="container col-6 mt-2 mb-2 text-center">
-        <div class="card p-4 bg-info">
-            <form method="POST" action="">
-              <h1 class="text-white bg-dark">Add student data</h1>
-                <input type="text" name="name" placeholder="Enter full name" class="form-control mt-2 mb-2" required>
-                <input type="email" name="email" placeholder="demo@gmail.com" class="form-control mt-2 mb-2" required>
-                <input type="number" name="phone" placeholder="Enter phone number" class="form-control mt-2 mb-2" required>
-                <input type="text" name="address" placeholder="Enter address" class="form-control mt-2 mb-2 p-4" required>
-                <input type="submit" name="submit" value="Add Student" class="btn btn-primary mt-2 mb-2">
-
-            </form>
-        </div>
-    </div>
-
+    <div class="parent">
+    <div class="home-page-ka-header-div"></div>
+    <div class="notice-board-div"></div>
+    <div class="role-switch-karne-ka-slider-div"></div>
+    <div class="login-details-div"></div>
+    <div class="login-details-submit-button-wala-div"></div>
+    <div class="forgot-password-div"></div>
+    <div class="ai-chatbot-wala-div"></div>
+    <div class="educore-ka-naam-aur-version-div"></div>
+</div>
+    
     <!-- Optional JavaScript; choose one of the two! -->
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
